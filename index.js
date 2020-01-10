ff850a74bdabed9789ee8e1b0c8d9be4d0a85338
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-01-10 23:47:38 +0530

    V1.0

---
+++ index.js
@@ -0,0 +1,12 @@
+for( var i = 0 ; i < document.querySelectorAll(".drum").length ; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    
+    this.style.color = "white";
+    //What to do when click detected.
+});
+
+}
+
+// var audio = new Audio('./sounds/tom-1.mp3');
+// audio.play();
